f235d1a3793ad96cc8db9a7cb9c70faf90a10dc3
mtcnxd <mtc.nxd@gmail.com>
2026-05-27 14:49:38 -0600

    Foreach removed in notificator change

--- Jobs/NotificatorChange.py
+++ Jobs/NotificatorChange.py
@@ -25,26 +25,25 @@ with SessionLocal() as session:
 
         console.print(Panel(f"Average price: {average:.2f}"))
 
-        results = session.query(Ticker)\
+        result = session.query(Ticker)\
             .filter(Ticker.book == "btc_usdt")\
             .order_by(Ticker.id.desc())\
             .limit(1)\
-            .all()
+            .first()
 
-        for result in results:
-            falling_percentage = ((result.last - result.high) / result.high) * 100
-            raising_percentage = ((result.last - result.low) / result.low) * 100
+        falling_percentage = ((result.last - result.high) / result.high) * 100
+        raising_percentage = ((result.last - result.low) / result.low) * 100
 
-            console.print(f"{result.created_at} | Low: {result.low} - Current: {result.last} - High: {result.high} | From low to last: {raising_percentage:.2f}% | From high to last: {falling_percentage:.2f}%")
+        console.print(f"{result.created_at} | Low: {result.low} - Current: {result.last} - High: {result.high} | From low to last: {raising_percentage:.2f}% | From high to last: {falling_percentage:.2f}%")
 
-            Telegram().send_message(
-                f"*Updated at:* {result.created_at} \n"
-                f"*Lower price:* {to_currency(result.low)} \n"
-                f"*Current price:* {to_currency(result.last)} \n"
-                f"*Higher price:* {to_currency(result.high)} \n"
-                f"*From low to current:* {to_percentage(raising_percentage)} \n"
-                f"*From high to current:* {to_percentage(falling_percentage)} \n"
-                f"*Average price:* {to_currency(average)}")
+        Telegram().send_message(
+            f"*Updated at:* {result.created_at} \n"
+            f"*Lower price:* {to_currency(result.low)} \n"
+            f"*Current price:* {to_currency(result.last)} \n"
+            f"*Higher price:* {to_currency(result.high)} \n"
+            f"*From low to current:* {to_percentage(raising_percentage)} \n"
+            f"*From high to current:* {to_percentage(falling_percentage)} \n"
+            f"*Average price:* {to_currency(average)}")
 
     except Exception as e:
         console.print(f"ERROR: {e}")
